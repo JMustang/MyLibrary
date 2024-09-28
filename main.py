90fdced4b9f9d51dbb4d00ce414dd588d0d4b94e
Junior <eecfredes@gmail.com>
2024-09-28 20:15:49 -0300

    :alembic:Learning about route, path and methods

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello World!"}
+
+
+@app.get("/greet")
+async def greet(name: Optional[str] = "User", age: int = 0) -> dict:
+    return {"message": f"Ola {name}", "age": f"age: {age}"}
+
+
+@app.post("/create_book")
+async def create_book():
+    pass
