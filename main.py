3225c8507361d5eedcca97717407fc6dae6e9d6a
Junior <eecfredes@gmail.com>
2024-09-30 21:13:57 -0300

    :rocket:Get book by id

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ async def get_books():
     return db.books
 
 
+@app.get("/books/{book_id}")
+async def get_book(book_id: int):
+    return db.books[book_id]
+
+
 @app.post("/books")
 async def create_books():
     db.books.append(book)
