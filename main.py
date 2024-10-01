eb30d859066206027a1b112f5f8387305c614220
Junior <eecfredes@gmail.com>
2024-10-01 20:48:34 -0300

    :rocket:Create books

--- main.py
+++ main.py
@@ -16,26 +16,35 @@ class Book(BaseModel):
     language: str
 
 
+# GET
 @app.get("/books", response_model=list[Book])
 async def get_books():
     return db.books
 
 
+# GET BY ID
 @app.get("/book/{book_id}")
 async def get_book(book_id: int) -> dict:
     return db.books[book_id]
 
 
+# POST
 @app.post("/books")
-async def create_books() -> dict:
-    db.books.append(book)
+async def create_books(book_data: int) -> dict:
+    new_book = book_data.model_dump()
+
+    db.books.append(new_book)
+
+    return new_book
 
 
+# UPDATE
 @app.patch("/book/{book_id}")
 async def update_book(book_id: int) -> dict:
     return db.books[book_id]
 
 
+# DELETE
 @app.delete("/book/{book_id}")
 async def delete_book(book_id: int) -> dict:
     return db.books[book_id]
