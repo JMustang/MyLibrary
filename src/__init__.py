b6589b985fa2752e917845d23f1d9908c344f8cb
Junior <eecfredes@gmail.com>
2024-10-10 21:06:15 -0300

    :rocket:Establishing the connection to the database

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from src.books.routes import router as books_router
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print("Application starting up...")
+    yield
+    print("Application shutting down...")
 
 
 version = "v1"
@@ -8,6 +16,7 @@ app = FastAPI(
     version=version,
     title="My Library",
     description="A REST API for a book review web service",
+    lifespan=life_span,
 )
 
 app.include_router(books_router, prefix=f"/api/{version}/books", tags=["books"])
