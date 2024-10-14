60f0eac285c8efa8d128c6f63930c8ce10e76747
Junior <eecfredes@gmail.com>
2024-10-14 20:58:37 -0300

    :rocket: Changing the book model a bit

--- src/books/models.py
+++ src/books/models.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
-from datetime import datetime
+from datetime import date, datetime
 import uuid
 
 
@@ -14,11 +14,11 @@ class Book(SQLModel, table=True):
     title: str
     author: str
     publisher: str
-    published_date: str
+    published_date: date
     page_count: int
     language: str
-    created_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
-    updated_at: datetime = Field(Column(pg.TIMESTAMP, default=datetime.now))
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
 
     def __repr__(self):
         return f"<Book {self.title}>"
