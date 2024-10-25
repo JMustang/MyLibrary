5f6e3333cfbc32a49328bc8160a58039a5fb5957
Junior <eecfredes@gmail.com>
2024-10-24 21:37:13 -0300

    :recycle:Rename file

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from .schemas import UserCreateModel
 
 authRouter = APIRouter(prefix="/signup", tags=["auth"])
 
 
 @authRouter.post("/")
-async def create_user_account():
+async def create_user_account(user_date: UserCreateModel):
     pass
