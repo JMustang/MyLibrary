65a72ea4d272d62fad7d0c9500cd2a102f600972
Junior <eecfredes@gmail.com>
2024-10-24 21:49:24 -0300

    :rocket:create user service

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,4 +1,5 @@
 from .models import User
+from .schemas import UserCreateModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 
@@ -13,3 +14,7 @@ class UserService:
     async def user_exists(self, email, session: AsyncSession):
         user = await self.get_user_by_email(email, session)
         return True if user is not None else False
+
+    async def create_user(self, user_data: UserCreateModel, session: AsyncSession):
+        user_data_dict = user_data.model_dump()
+        new_user = User(**user_data_dict)
