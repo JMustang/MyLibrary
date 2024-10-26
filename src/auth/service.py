935416d6a8568de0f330b337527e019732004908
Junior <eecfredes@gmail.com>
2024-10-26 19:36:52 -0300

    :rocket:create user service

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,5 +1,6 @@
 from .models import User
 from .schemas import UserCreateModel
+from .utils import gen_passwd_hash
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 
@@ -18,3 +19,7 @@ class UserService:
     async def create_user(self, user_data: UserCreateModel, session: AsyncSession):
         user_data_dict = user_data.model_dump()
         new_user = User(**user_data_dict)
+        new_user.password_hash = gen_passwd_hash(user_data_dict["password"])
+        session.add(new_user)
+        await session.commit()
+        return new_user
