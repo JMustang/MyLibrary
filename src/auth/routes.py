ae93c47ec5a9278835b892a0e91f6fc7bd230c95
Junior <eecfredes@gmail.com>
2024-10-26 19:36:02 -0300

    :rocket: Create a sing up router

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,9 +1,28 @@
-from fastapi import APIRouter
-from .schemas import UserCreateModel
+from fastapi import APIRouter, Depends, status
+from .schemas import UserCreateModel, UserModel
+from .service import UserService
+from src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from fastapi.exceptions import HTTPException
 
-authRouter = APIRouter(prefix="/signup", tags=["auth"])
+authRouter = APIRouter()
+user_service = UserService()
 
 
-@authRouter.post("/")
-async def create_user_account(user_date: UserCreateModel):
-    pass
+@authRouter.post(
+    "/singup", response_model=UserModel, status_code=status.HTTP_201_CREATED
+)
+async def create_user_account(
+    user_data: UserCreateModel, session: AsyncSession = Depends(get_session)
+):
+    email = user_data.email
+    user_exists = await user_service.user_exists(email, session)
+
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User with this email already exists",
+        )
+
+    new_user = await user_service.create_user(user_data, session)
+    return new_user
