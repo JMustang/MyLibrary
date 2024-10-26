fee434cf9853d8ec88a7db353800f5741481e706
Junior <eecfredes@gmail.com>
2024-10-26 19:36:23 -0300

    :rocket: user model schema

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
+import uuid
+from datetime import datetime
 
 
 class UserCreateModel(BaseModel):
     username: str = Field(min_length=3, max_length=50)
     email: str = Field(min_length=3, max_length=50)
     password: str = Field(min_length=3, max_length=50)
+
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
