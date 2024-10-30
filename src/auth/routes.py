a156da24a6814a84dac913e8ed85e5f58066a847
Junior <eecfredes@gmail.com>
2024-10-30 20:21:04 -0300

    :rocket:user login router

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter, Depends, status
-from .schemas import UserCreateModel, UserModel
+from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi.exceptions import HTTPException
+from .utils import create_access_token, decode_token, verify_passwd
+from datetime import timedelta
 
 authRouter = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRY = 2
 
 
 @authRouter.post(
@@ -26,3 +30,27 @@ async def create_user_account(
 
     new_user = await user_service.create_user(user_data, session)
     return new_user
+
+
+@authRouter.post("/login")
+async def login_users(
+    login_data: UserLoginModel, session: AsyncSession = Depends(get_session)
+):
+    email = login_data.email
+    password = login_data.password
+
+    user = await user_service.get_user_by_email(email, session)
+
+    if user is not None:
+        password_valid = verify_passwd(password, user.password_hash)
+
+        if password_valid:
+            access_token = create_access_token(
+                user_data={"email": user.email, "user_uid": str(user.uid)}
+            )
+
+            refresh_token = create_access_token(
+                user_data={"email": user.email, "user_uid": str(user.uid)},
+                expiry=timedelta(days=REFRESH_TOKEN_EXPIRY),
+                refresh=True,
+            )
