00211b263928e9f80aac51e3ec48e51f3f776789
Junior <eecfredes@gmail.com>
2024-11-02 23:22:26 -0300

    :rocket:inject access token bearer

--- src/books/routes.py
+++ src/books/routes.py
@@ -17,13 +17,18 @@ async def get_books(
     session: AsyncSession = Depends(get_session),
     user_details=Depends(access_token_bearer),
 ):
+    print("user_details", user_details)
     db = await book_service.get_all_books(session)
     return db
 
 
 # GET BY ID
 @router.get("/{book_uid}", response_model=Book)
-async def get_book(book_uid: str, session: AsyncSession = Depends(get_session)) -> dict:
+async def get_book(
+    book_uid: str,
+    session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
+) -> dict:
     book = await book_service.get_book_by_id(book_uid, session)
 
     if book:
@@ -37,7 +42,9 @@ async def get_book(book_uid: str, session: AsyncSession = Depends(get_session)) 
 # POST
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
 async def create_books(
-    book_data: CreateBookModel, session: AsyncSession = Depends(get_session)
+    book_data: CreateBookModel,
+    session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
 ) -> dict:
     new_book = await book_service.create_book(book_data, session)
 
@@ -50,6 +57,7 @@ async def update_book(
     book_uid: str,
     updateBook: UpdateBookModel,
     session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
 ) -> dict:
     update_book = await book_service.update_book(book_uid, updateBook, session)
 
@@ -64,7 +72,9 @@ async def update_book(
 # DELETE
 @router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book_by_id(
-    book_uid: str, session: AsyncSession = Depends(get_session)
+    book_uid: str,
+    session: AsyncSession = Depends(get_session),
+    user_details=Depends(access_token_bearer),
 ):
     book = await book_service.delete_book(book_uid, session)
 
