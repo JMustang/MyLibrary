226d666f9cb0ee171c96618e225fed73eda1d15d
Junior <eecfredes@gmail.com>
2024-11-02 22:14:10 -0300

    :rocket: created class Access token bearer

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,5 +1,15 @@
+from fastapi import Request
 from fastapi.security import HTTPBearer
+from fastapi.security.http import HTTPAuthorizationCredentials
 
 
 class AccessTokenBearer(HTTPBearer):
-    pass
+
+    def __init__(self, auto_error=True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+        creds = await super().__call__(request)
+        print(creds.scheme)
+        print(creds.credentials)
+        return creds
